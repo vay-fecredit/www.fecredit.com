f6d1a83583e26b6762cdec30ecc250d86cc6fc2d
www.fecredit.com <shinnhanfinnance@gmail.com>
2025-11-22 06:12:40 +0700

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'no-unused-vars': ['warn', { 
+      'argsIgnorePattern': '^_',
+      'varsIgnorePattern': '^_'
+    }],
+    'no-console': ['warn', { 
+      allow: ['warn', 'error'] 
+    }],
+    'prefer-const': 'error',
+    'no-var': 'error',
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    'build/',
+    'coverage/',
+    '*.min.js',
+    'web-sdk-version-3.2.0.0/',
+    'lib/',
+    'ekyc-mock/',
+    'models/',
+    'public/js/configs/',
+  ],
+};
